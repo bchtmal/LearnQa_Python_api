d7d87cee0fe7a2d499859a6f3c6c5b781ad4cde9
HOME-PC\alex <bchtmal@gmail.com>
2022-12-04 15:08:42 +0300

    ответ на гет запрос

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print('Hello from, Alex')
+import requests
+
+# print('Hello from Alex') # первое задание
+
+response = requests.get('https://playground.learnqa.ru/api/get_text')
+print(response.text)

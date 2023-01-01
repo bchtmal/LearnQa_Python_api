a791747de21e6469ec6443a35ba8242ed456836c
HOME-PC\alex <bchtmal@gmail.com>
2023-01-01 19:03:57 +0300

    ex16

---
+++ tests/test_user_get.py
@@ -0,0 +1,61 @@
+import allure
+from lib.my_requests import MyRequests
+from lib.base_case import BaseCase
+from lib.assertions import Assertions
+
+@allure.epic("Get user details cases")
+class TestUserGet(BaseCase):
+
+    @allure.description("This test is try to get user details without auth")
+    def test_get_user_details_not_auth(self):
+        response = MyRequests.get("/user/2")
+
+        Assertions.assert_json_has_key(response, 'username')
+        Assertions.assert_json_has_not_key(response, 'email')
+        Assertions.assert_json_has_not_key(response, 'firstName')
+        Assertions.assert_json_has_not_key(response, 'lastName')
+
+    @allure.description("This test is gets user details as a same user")
+    def test_get_user_details_auth_as_same_user(self):
+        data = {
+            'email': 'vinkotov@example.com',
+            'password': '1234'
+        }
+
+        response1 = MyRequests.post("/user/login", data=data)
+
+        auth_sid = self.get_cookie(response1, 'auth_sid')
+        token = self.get_header(response1, 'x-csrf-token')
+        user_id_from_auth_method = self.get_json_value(response1, 'user_id')
+
+        response2 = MyRequests.get(f"/user/{user_id_from_auth_method}",
+                                 headers={'x-csrf-token': token},
+                                 cookies={'auth_sid': auth_sid}
+                                 )
+
+        expected_fields = ['username', 'email', 'firstName', 'lastName']
+        Assertions.assert_json_has_keys(response2, expected_fields)
+
+    @allure.description("This test is get user details by anoughter user")
+    def test_user_gets_anoughter_user_details(self):
+        data = {
+            'email' : 'vinkotov@example.com',
+            'password': '1234'
+        }
+
+        response1 = MyRequests.post("/user/login", data = data)
+
+        auth_sid = self.get_cookie(response1,"auth_sid")
+        token = self.get_header(response1,"x-csrf-token")
+        user_id_from_auth_method = self.get_json_value(response1,"user_id") - 1
+
+        response2 = MyRequests.get(
+            f"/user/{user_id_from_auth_method}",
+            headers={"x-csrf-token" : token},
+            cookies={"auth_sid" : auth_sid}
+        )
+
+        Assertions.assert_json_has_key(response2, "username")
+        Assertions.assert_json_has_not_key(response2, "email")
+        Assertions.assert_json_has_not_key(response2, "firstName")
+        Assertions.assert_json_has_not_key(response2, "lastName")
